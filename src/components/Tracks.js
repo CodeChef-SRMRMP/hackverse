f166b7baf0b813f5123b3c15a781bd59004b8300
Chanakyha <vetrichanakyha2003@gmail.com>
2023-02-22 18:43:26 +0530

    Added Sponsor Track

--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -53,21 +53,33 @@ const Tracks = () => {
       <p className="text-white font-bold mt-10 mx-auto w-full md:w-1/3 text-center bg-red-500/30 backdrop-blur-md px-3 py-3 text-lg rounded-md">
         {description}
       </p>
-      <div className="bg-black/60 mt-5 flex flex-wrap gap-5 items-end justify-center md:justify-between lg:justify-evenly px-10 lg:px-20 backdrop-blur-xl rounded-md text-white p-10">
-        {tracks?.map(({ name, desc }) => {
-          return (
-            <h1
-              onMouseEnter={() => {
-                setDescription(desc);
-              }}
-              onMouseLeave={() =>
-                setDescription('General description for all tracks')
-              }
-              className="hover:bg-yellow-600 cursor-pointer border-2 text-center px-3 py-2 text-4xl rounded-xl w-full md:w-1/3 border-white">
-              {name}
-            </h1>
-          );
-        })}
+      <div className="bg-black/60 mt-5 p-10 backdrop-blur-xl px-10 lg:px-20">
+        <div className="flex flex-wrap gap-5 items-end justify-center md:justify-between lg:justify-evenly rounded-md text-white">
+          {tracks?.map(({ name, desc }) => {
+            return (
+              <h1
+                onMouseEnter={() => {
+                  setDescription(desc);
+                }}
+                onMouseLeave={() =>
+                  setDescription('General description for all tracks')
+                }
+                className="hover:bg-yellow-600 cursor-pointer border-2 text-center px-3 py-2 text-4xl rounded-xl w-full md:w-1/3 border-white">
+                {name}
+              </h1>
+            );
+          })}
+        </div>
+        <h1
+          onMouseEnter={() => {
+            setDescription('Description for Sponsor Track');
+          }}
+          onMouseLeave={() =>
+            setDescription('General description for all tracks')
+          }
+          className="w-2/3 mx-auto hover:bg-transparent hover:text-white border-2 border-black hover:border-white hover:animate-none bg-yellow-600 mt-10 px-4 py-2 rounded-lg text-4xl cursor-pointer shadow-black shadow-xl animate-pulse text-center">
+          Sponsor Track
+        </h1>
       </div>
     </div>
   );
