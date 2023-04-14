7d38253a40e70de58f72d7c8a0eedc08277b191e
Chanakyha <vetrichanakyha2003@gmail.com>
2023-04-14 22:57:26 +0530

    Fixed Community Partners

--- src/components/CommunityPartnerCard.js
+++ src/components/CommunityPartnerCard.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import Marquee from 'react-fast-marquee';
 
+function importAll(r) {
+  let images = {};
+  r.keys().map((item, index) => {
+    images[item.replace('./', '')] = r(item);
+  });
+  return images;
+}
+
+const images = importAll(
+  require.context(
+    '../assets/images/sponsors/community/',
+    false,
+    /\.(png|jpe?g|svg)$/
+  )
+);
+
 const CommunityPartnerCard = () => {
   return (
     <>
@@ -10,18 +26,9 @@ const CommunityPartnerCard = () => {
       <div className="m-4 rounded-lg bg-[#202021]/60 backdrop-blur-3xl mx-5 md:mx-10 lg:mx-20 p-5 py-7 text-white dark:text-black dark:shadow-gray-500">
         <Marquee pauseOnHover gradient={false} speed={100}>
           <div className="flex items-center space-x-20 mx-10">
-            {Array(23)
-              .fill(1)
-              .map((_, i) => (
-                <img
-                  key={i}
-                  className="w-32"
-                  src={require(`../assets/images/sponsors/community/${
-                    i + 1
-                  }.png`)}
-                  alt=""
-                />
-              ))}
+            {Object.values(images).map((image, i) => (
+              <img key={i} className="w-32" src={image} alt="" />
+            ))}
           </div>
         </Marquee>
       </div>
